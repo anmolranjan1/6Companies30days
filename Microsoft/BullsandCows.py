3717db10058e720d2098c7631e50124835b406c8
Anmol Ranjan <anmolranjan1@gmail.com>
2023-01-03 15:53:36 +0530

    Correction

---
+++ Microsoft/BullsandCows.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        b = c = 0
+        bf = [0 for i in range(10)]
+        cf = [0 for i in range(10)]
+        for i in range(len(secret)):
+            if secret[i] == guess[i]:
+                b += 1
+            else:
+                bf[int(secret[i])] += 1
+                cf[int(guess[i])] += 1
+       # print(bf,cf)
+        for i in range(10):
+            c += min(bf[i], cf[i])
+        return str(b)+"A"+str(c)+"B"
