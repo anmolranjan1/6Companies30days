b26abb1ecd2d64a22e3b29c6ce8bbdac1cedc5bb
Anmol Ranjan <anmolranjan1@gmail.com>
2023-01-04 09:12:32 +0530

    Second commit

--- Microsoft/EvaluateReversePolishNotation.py
+++ Microsoft/EvaluateReversePolishNotation.py
@@ -2,7 +2,7 @@ class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stk = []
         for x in tokens:
-            if x not in ["+", "-", "/", "*"]:
+            if x not in ["+","-","/","*"]:
                 stk.append(int(x))
             else:
                 a = stk.pop(-1)
