c57aedaea067ec0a071ab40a9f90c28f082940b2
Anmol Ranjan <anmolranjan1@gmail.com>
2023-01-24 10:03:19 +0530

    Day 20

---
+++ Flipkart/791. Custom Sort String.py
@@ -0,0 +1,8 @@
+class Solution:
+    def customSortString(self, S: str, T: str) -> str:
+        ans = ""
+        for i in S:
+            if i in T:
+                ans += (i * T.count(i)) 
+                T = T.replace(i, '')
+        return ans+Ts
